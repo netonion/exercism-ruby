e4b767209efbd3c8d049e54d59081789bb5b2fc2
Sam Guo <netonion@hotmail.com>
2017-08-16 14:42:07 -0400

    Phone number using regex

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -1,8 +1,7 @@
 module PhoneNumber
   def self.clean(num)
     cleaned = num.tr('^0-9', '')
-    cleaned = cleaned[1..-1] if cleaned.size == 11 && cleaned.start_with?('1')
-    cleaned.size == 10 && ('2'..'9').cover?(cleaned[0]) && ('2'..'9').cover?(cleaned[3]) ? cleaned : nil
+    cleaned.match(/^1?([2-9]\d{2}[2-9]\d{6})$/) ? $1 : nil
   end
 end
 
