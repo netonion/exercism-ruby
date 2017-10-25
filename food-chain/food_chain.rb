c96c0e27fb755feca8a6bc4a22afa14b6612b9d3
Sam Guo <netonion@hotmail.com>
2017-10-25 00:01:55 -0400

    Food chain generated programmatically

--- food-chain/food_chain.rb
+++ food-chain/food_chain.rb
@@ -1,57 +1,24 @@
 module FoodChain
+  ANIMALS = %w(fly spider bird cat dog goat cow)
+  COMMENTS = [
+    "",
+    "It wriggled and jiggled and tickled inside her.\n",
+    "How absurd to swallow a bird!\n",
+    "Imagine that, to swallow a cat!\n",
+    "What a hog, to swallow a dog!\n",
+    "Just opened her throat and swallowed a goat!\n",
+    "I don't know how she swallowed a cow!\n"
+  ]
+
+  # Each verse consists of two parts - the first two lines (part 1) and the rest (part 2)
+  # Each part 2 is the previous part 2 with a new line prepended to it
   def self.song
-    <<-EOM
-I know an old lady who swallowed a fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-
-I know an old lady who swallowed a spider.
-It wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-
-I know an old lady who swallowed a bird.
-How absurd to swallow a bird!
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-
-I know an old lady who swallowed a cat.
-Imagine that, to swallow a cat!
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-
-I know an old lady who swallowed a dog.
-What a hog, to swallow a dog!
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-
-I know an old lady who swallowed a goat.
-Just opened her throat and swallowed a goat!
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-
-I know an old lady who swallowed a cow.
-I don't know how she swallowed a cow!
-She swallowed the cow to catch the goat.
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-
-I know an old lady who swallowed a horse.
-She's dead, of course!
-    EOM
+    verses, rest = "", "I don't know why she swallowed the fly. Perhaps she'll die.\n\n"
+    7.times do |i|
+      verses.concat("I know an old lady who swallowed a #{ANIMALS[i]}.\n#{COMMENTS[i]}").concat(rest)
+      rest.prepend "She swallowed the #{ANIMALS[i + 1]} to catch the #{ANIMALS[i]}#{ ' that wriggled and jiggled and tickled inside her' if i == 1}.\n"
+    end
+    verses + "I know an old lady who swallowed a horse.\nShe's dead, of course!\n"
   end
 end
 
